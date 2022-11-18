2acd0ff3184cd78bc0caa28bdfc5b422082cab6b
Pau Sabé <pau.sabe@icloud.com>
2022-11-18 10:10:58 +0100

    Fixing Switch settings problem

--- src/Components/SettingsComponent.js
+++ src/Components/SettingsComponent.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {View, Text, Switch, StyleSheet, Platform} from 'react-native';
-import { Picker as SelectPicker } from '@react-native-picker/picker';
+import {Picker as SelectPicker} from '@react-native-picker/picker';
 import Slider from '@react-native-community/slider';
-
 import HR from './HRComponent';
-import GLOBAL from "../Utils/GlobalKeys";
+import GlobalKeys from "../Utils/GlobalKeys";
 
-export default class SettingsComponent extends Component{
+export default class SettingsComponent extends Component {
 
-    constructor(props){
+    constructor(props) {
         super();
         this.name = props.name;
         this.description = props.description;
@@ -20,14 +19,14 @@ export default class SettingsComponent extends Component{
         this.selectorProps = props.selectorProps;
     }
 
-    UNSAFE_componentWillMount(){
+    UNSAFE_componentWillMount() {
         this.setState({
             value: this.value,
             Aa: this.convertTextSize(this.value),
         });
     }
 
-    render(){
+    render() {
         let selectorComponent;
         let styleSelectorView = styles.selectorView;
         switch (this.props.selectorComponent) {
@@ -45,78 +44,79 @@ export default class SettingsComponent extends Component{
             default:
         }
 
-        return(
-          <View>
-              <View style={styles.option}>
-                  <View style={styles.textView}>
-                      <Text style={styles.text}>{this.name}</Text>
-                  </View>
-                  <View style={styleSelectorView}>
-                      {selectorComponent}
-                  </View>
-              </View>
-              <HR/>
-          </View>
+        return (
+            <View>
+                <View style={styles.option}>
+                    <View style={styles.textView}>
+                        <Text style={styles.text}>{this.name}</Text>
+                    </View>
+                    <View style={styleSelectorView}>
+                        {selectorComponent}
+                    </View>
+                </View>
+                <HR/>
+            </View>
         );
     }
 
-    _generateSwitch(){
+    _generateSwitch() {
         let selectorProps = this._mergeProps({
-            value: this.state.value,
-            trackColor:{true: GLOBAL.switchColor, false: 'bababa'},
+            value: !!this.state.value,
+            trackColor: {true: GlobalKeys.switchColor},
             onValueChange: this._updateSelectionStateCallback.bind(this),
         });
         return React.createElement(Switch, selectorProps);
     }
 
-    _generateSlider(){
+    _generateSlider() {
         let selectorProps = this._mergeProps({
             value: this.state.value,
-            minimumTrackTintColor: GLOBAL.switchColor,
+            minimumTrackTintColor: GlobalKeys.switchColor,
             onSlidingComplete: this._selectionCallback.bind(this)
         });
         return (
-           <View style={{flex: 1, flexDirection: 'row',}}>
-            <View style={{flex: 3, paddingRight: 10, justifyContent: 'center'}}>
-              {React.createElement(Slider, selectorProps)}
+            <View style={{flex: 1, flexDirection: 'row',}}>
+                <View style={{flex: 3, paddingRight: 10, justifyContent: 'center'}}>
+                    {React.createElement(Slider, selectorProps)}
+                </View>
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <Text style={{
+                        textAlign: 'center',
+                        color: 'black',
+                        fontSize: this.state.Aa,
+                        fontWeight: '300'
+                    }}>
+                        {'Aa'}
+                    </Text>
+                </View>
             </View>
-            <View style={{flex: 1, justifyContent: 'center'}}>
-              <Text style={{textAlign: 'center',
-                            color: 'black',
-                            fontSize: this.state.Aa,
-                            fontWeight: '300'}}>
-                  {'Aa'}
-              </Text>
-            </View>
-          </View>
         );
     }
 
-    _generatePicker(){
-        if(Platform.OS === 'ios'){
-          let selectorProps = this._mergeProps({
-              selectedValue: this.state.value,
-              onValueChange: this._updateSelectionStateCallback.bind(this)
-          });
-          return React.createElement(SelectPicker, selectorProps,
-              this._generatePickerOptions()
-          );
-        }
-        else{
-          let selectorProps = this._mergeProps({
-              selectedValue: this.state.value,
-              onValueChange: this._updateSelectionStateCallback.bind(this),
-              style: styles.selectorPicker
-          });
-          return React.createElement(SelectPicker, selectorProps,
-              this._generatePickerOptions()
-          );
+    _generatePicker() {
+        if (Platform.OS === 'ios') {
+            let selectorProps = this._mergeProps({
+                selectedValue: this.state.value,
+                onValueChange: this._updateSelectionStateCallback.bind(this)
+            });
+            return React.createElement(SelectPicker, selectorProps,
+                this._generatePickerOptions()
+            );
+        } else {
+            let selectorProps = this._mergeProps({
+                selectedValue: this.state.value,
+                onValueChange: this._updateSelectionStateCallback.bind(this),
+                style: styles.selectorPicker
+            });
+            return React.createElement(SelectPicker, selectorProps,
+                this._generatePickerOptions()
+            );
         }
     }
 
-    _generatePickerOptions(){
+    _generatePickerOptions() {
         let options = [];
-        for(let key in this.options){
+        for (let key in this.options) {
             options.push(
                 <SelectPicker.Item label={this.options[key]} value={key} key={key}/>
             );
@@ -124,57 +124,47 @@ export default class SettingsComponent extends Component{
         return options;
     }
 
-    _updateSelectionStateCallback(value){
+    _updateSelectionStateCallback(value) {
         this.setState({
             value: value
         });
         this._selectionCallback(value);
     }
 
-    _selectionCallback(value){
-      var textSize = this.convertTextSize(Math.trunc(value));
+    _selectionCallback(value) {
+        const textSize = this.convertTextSize(Math.trunc(value));
         this.setState({
             Aa: textSize,
         });
         this.callback(this.id, value);
     }
 
-    convertTextSize(value){
-      switch (value) {
-        case 1:
-          return GLOBAL.size1;
-          break;
-        case 2:
-          return GLOBAL.size2;
-          break;
-        case 3:
-          return GLOBAL.size3;
-          break;
-        case 4:
-          return GLOBAL.size4;
-          break;
-        case 5:
-          return GLOBAL.size5;
-          break;
-        case 6:
-          return GLOBAL.size6;
-          break;
-        case 7:
-          return GLOBAL.size7;
-          break;
-        case 8:
-          return GLOBAL.size8;
-          break;
-        case 9:
-          return GLOBAL.size9;
-          break;
-        case 10:
-          return GLOBAL.size10;
-          break;
-      }
+    convertTextSize(value) {
+        switch (value) {
+            case 1:
+                return GlobalKeys.size1;
+            case 2:
+                return GlobalKeys.size2;
+            case 3:
+                return GlobalKeys.size3;
+            case 4:
+                return GlobalKeys.size4;
+            case 5:
+                return GlobalKeys.size5;
+            case 6:
+                return GlobalKeys.size6;
+            case 7:
+                return GlobalKeys.size7;
+            case 8:
+                return GlobalKeys.size8;
+            case 9:
+                return GlobalKeys.size9;
+            case 10:
+                return GlobalKeys.size10;
+        }
     }
 
-    _mergeProps(properties){
+    _mergeProps(properties) {
         return Object.assign(this.selectorProps ? this.selectorProps : {}, properties);
     }
 
